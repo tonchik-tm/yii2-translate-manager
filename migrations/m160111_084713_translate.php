501ba72657f8476881cf2fd3524146a846d8eca6
cmg_anton_nikolaev <anton.nikolaev@convergent-usa.com>
2016-01-11 12:01:49 +0300

    longtext for message

--- migrations/m160111_084713_translate.php
+++ migrations/m160111_084713_translate.php
@@ -8,10 +8,12 @@ class m160111_084713_translate extends Migration
     public function safeUp()
     {
         $this->alterColumn('{{language_source}}', 'message', 'longtext NULL');
+        $this->alterColumn('{{language_translate}}', 'message', 'longtext NULL');
     }
 
     public function safeDown()
     {
         $this->alterColumn('{{language_source}}', 'message', Schema::TYPE_TEXT . ' NULL');
+        $this->alterColumn('{{language_translate}}', 'message', Schema::TYPE_TEXT . ' NULL');
     }
 }
